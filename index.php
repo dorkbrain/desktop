c08ebf8b2a2a5df88ce9eff047e9cbdf6a2a1224
dorkbrain <dpcox95@gmail.com>
2022-08-21 22:20:38 -0400

    modified:   index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <?php
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
+  header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+  header("Cache-Control: post-check=0, pre-check=0", false);
+  header("Pragma: no-cache");
 ?>
 <html>
   <head>
